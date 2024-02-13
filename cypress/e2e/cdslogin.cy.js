9fd35879cc4d3f04aa3bedbab9bfaf54d17353e8
Asad Atiq <124272323+asadatiq1@users.noreply.github.com>
2024-02-13 13:08:21 +0500

    comment removal

--- cypress/e2e/cdslogin.cy.js
+++ cypress/e2e/cdslogin.cy.js
@@ -1,30 +1,22 @@
+import LoginPage from "../PageObject/cdslogin.cy";
+import cdslogin from "../fixtures/cdslogin.json";
 
-import LoginPage from '../PageObject/cdslogin.cy';
-import cdslogin from '../fixtures/cdslogin.json';
-
-describe('Login Test', () => {
+describe("Login Test", () => {
   beforeEach(() => {
-    // Assuming your application is served at http://localhost:3000
-        LoginPage.visitSite();
+    LoginPage.visitSite();
   });
 
-  it('should login with valid credentials', () => {
-
-    // Use methods from the LoginPage Page Object
+  it("should login with valid credentials", () => {
     LoginPage.typeUsername(cdslogin.email);
     LoginPage.typePassword(cdslogin.password);
     LoginPage.clickLoginButton();
-    // Add assertions as needed
     LoginPage.checkDashboard();
   });
 
-  it('should display an error with invalid credentials', () => {
-    // Use methods from the LoginPage Page Object
+  it("should display an error with invalid credentials", () => {
     LoginPage.typeUsername(cdslogin.email);
-    LoginPage.typePassword('wrong password');
+    LoginPage.typePassword("wrong password");
     LoginPage.clickLoginButton();
-    // Add assertions as needed
     LoginPage.checkErrorMsg();
   });
-
 });
